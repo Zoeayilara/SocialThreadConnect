82e8964d1a0f984221505ef5039b55b0ea6afcc8
Zoe Aayilara <zoeayilara@gmail.com>
2025-09-12 15:22:49 +0100

    Fix CORS: Add credentials header to Access-Control-Allow-Headers

--- server/index.ts
+++ server/index.ts
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
   }
   
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Cookie');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Cookie, credentials');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Expose-Headers', 'Set-Cookie');
   
